a967a8a1d6eb0e337c5898e146ef13f6f34ef2c8
JhonFDev <jhonfdeveloper@gmail.com>
2023-08-13 16:33:18 -0500

    arryas practica #1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let SelectedColor = ["red", "blue"];
 console.log(SelectedColor[0]);
 
-let hour = 20;
+let hour = 11;
 
 if (hour >= 6 && hour < 12) {
   console.log("good morning");
@@ -9,10 +9,7 @@ if (hour >= 6 && hour < 12) {
   console.log("good afternoon");
 } else console.log("good evening");
 
-
-
 // String Practica #1
-
 let miRefran;
 miRefran = "El que sabe, sabe; y el que no, aprende";
 
@@ -24,16 +21,39 @@ let indicePalabra = [miRefran.indexOf("otra")];
 
 if (indicePalabra >= 0) {
   miRefran = miRefran.replace("que", "xxx");
-}else
-
-if (indicePalabra < 0) {
-    indicePalabra = miRefran.indexOf("aprende");
-  miRefran = miRefran.replace("aprende", "xxx")
+} else if (indicePalabra < 0) {
+  indicePalabra = miRefran.indexOf("aprende");
+  miRefran = miRefran.replace("aprende", "xxx");
 }
 
+console.log(" ");
+console.log("String: Pratica #1");
 console.log(miRefran);
 console.log(longitudRefran);
 console.log(novenaLetra);
 console.log(ultimaaLetra);
 console.log(indicePalabra);
 console.log(miRefran.toUpperCase());
+console.log(" ");
+
+
+
+//Arrays Practica #1
+let myArray = [12, "hola", true];
+
+myArray[0] = "hamburguesa";
+myArray[1] = "piano";
+
+myArray.unshift = "sushi";
+
+var arrayString = myArray.join('-')
+console.log(" ");
+console.log("Arrays: Pratica #1");
+console.log(myArray);
+console.log("Arrays con guion");
+console.log(arrayString);
+console.log(" ");
+
+var mixedArray = ["Hola", 42, true, null, { nombre: "Juan", edad: 25 }, ["manzana", "naranja"]];
+console.log(mixedArray);
+console.log(" ");
